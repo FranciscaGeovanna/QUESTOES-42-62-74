7d050c1cdf9b48656d8f2ae0f1c9e26d2e1d04db
FranciscaGeovanna <115251617+FranciscaGeovanna@users.noreply.github.com>
2022-12-05 11:48:44 -0300

    Add files via upload

---
+++ Questão 62.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+    setlocale (LC_ALL, "Portuguese_Brazil");
+    
+    int alunos, x;
+    float notas, tn = 0, media;
+    
+    printf ("Programa para calcular média");
+    
+    printf ("\nInforme o número de alunos: ");
+    scanf ("%d", &alunos);
+    
+    for (x = 1; x <= alunos; x++){
+        printf ("\nInforme as notas dos alunos: ");
+        scanf ("%f", &notas);
+        
+        tn = tn + notas; 
+    }
+       
+        media = tn/alunos;
+
+    printf ("\nMédia: %.2f", media);
+    
+    return 0;
+}
+
+
